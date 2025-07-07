536c7f728d2988dc2f5c7dbe783ad90c7b676c44
Antismart <timbwamoses83@gmail.com>
2025-07-07 10:00:21 +0300

    config: Update Vite environment types

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_WEATHERXM_API_KEY: string
+  readonly VITE_FLOW_ACCESS_NODE: string
+  readonly VITE_FLOW_NETWORK: string
+  readonly VITE_INSURANCE_POOL_ADDRESS: string
+  readonly VITE_ORACLE_CONTRACT_ADDRESS: string
+  readonly VITE_XINSURE_CONTRACT_ADDRESS: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
